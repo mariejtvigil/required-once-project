afd92cd259de80f909eab34940ef5a8b1cb90c95
Marie Vigil <mjkvigil@gmail.com>
2015-08-13 10:17:02 -0600

    started all over

--- index.php
+++ index.php
@@ -1,43 +1,35 @@
 <?php
-/**
- * User: Marie@jtdesignsolutions.com
- * Date: 8/13/15
- *
- */
 
-<html>
-
-	<head>
-
-	required_once (dirname (_DIR_)  . “/head-utils.php”);
-
-	equired_once (dirname (_DIR_)  . “/header.php”); ?>
-
-	</head>
+$PAGE_TITLE = "required-once-project";
+require_once("lib/head-utils.php");
 
 
- 		<body>
+<div class="container">
 
+//-------- HEADER --------
 
+	<header>
+		<?php require_once("lib/header.php"); ?>
+	</header>
 
+	<div class="row">
+		<section class="side-panel col-md-3">
+			<?php require_once("lib/side-panel.php"); ?>
+		</section>
 
-		</body>
+		<section class="main-content col-md-9">
+			<h1>Hello World</h1>
+			<p>This is the main page content here.</p>
+		</section>
+	</div>
 
-
-	<footer>
-
-		<?php required_once (dirname (_DIR_)  . “/footer.php”); ?>
-
-
+	<footer class="footer">
+		<?php require_once("lib/footer.php"); ?>
 	</footer>
+</div><!-- .container -->
 
-
+</body>
 </html>
-
-
-
-
-
 
 
 ?>
